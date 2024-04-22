e163a9546aac7eb8395f5211c96bfc04e9a46219
nimaxnimax <avaroti.service@gmail.com>
2024-04-22 18:58:41 +0100

    done

--- Secure_Shell/05_ssh_port_config.sh
+++ Secure_Shell/05_ssh_port_config.sh
@@ -9,13 +9,30 @@ if ! [[ "$NEW_PORT" =~ ^[0-9]+$ ]]; then
     exit 1
 fi
 
+# Uncomment the Port line in sshd_config if it's commented
+if grep -qE '^#\s*Port' /etc/ssh/sshd_config; then
+    sudo sed -i "s/^#\s*Port/Port/" /etc/ssh/sshd_config
+fi
+
+# Get the current SSH port number from sshd_config
+CURRENT_PORT=$(awk '$1=="Port" {print $2}' /etc/ssh/sshd_config)
+
 # Change SSH port in sshd_config
-sudo sed -i "s/Port 22/Port $NEW_PORT/" /etc/ssh/sshd_config
+sudo sed -i "s/^Port .*/Port $NEW_PORT/" /etc/ssh/sshd_config
 
 # Restart SSH service
 sudo systemctl restart ssh
 
-# Print status message
-echo "SSH port has been changed to $NEW_PORT."
-echo "SSH service has been restarted."
+# Add the new SSH port to UFW
+sudo ufw allow $NEW_PORT/tcp
 
+# Enable UFW
+sudo ufw --force enable
+
+# Print status messages
+echo "SSH port has been changed from $CURRENT_PORT to $NEW_PORT."
+echo "SSH service has been restarted."
+echo "The new port $NEW_PORT has been added to UFW."
+echo "UFW has been enabled."
+echo "Current UFW rules:"
+sudo ufw status numbered
