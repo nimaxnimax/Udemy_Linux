e1f198f9f5e1cbd2291d3cb23456cb6b7596e773
nimaxnimax <avaroti.service@gmail.com>
2024-04-22 18:42:54 +0100

    done

--- Secure_Shell/04_top_memory_process.sh
+++ Secure_Shell/04_top_memory_process.sh
@@ -6,14 +6,6 @@ NUM_PROCESSES=5
 # Get top memory-consuming processes
 top_processes=$(ps aux --sort -%mem | head -n $((NUM_PROCESSES + 1)) | tail -n $NUM_PROCESSES)
 
-# Print header
-echo "Top $NUM_PROCESSES Memory-Consuming Processes on Ubuntu:"
-echo "-------------------------------------------------------"
-echo "USER       PID %MEM    RSS    COMMAND"
-
-# Loop through each line of top processes
-while read -r line; do
-    # Print process details
-    echo "$line"
-done <<< "$top_processes"
+# Print top processes
+echo "$top_processes"
 
