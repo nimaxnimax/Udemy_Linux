9dd95c3a9e9fc686a841a2fd534a4a6d59bbdfa9
nimaxnimax <avaroti.service@gmail.com>
2024-04-22 18:44:39 +0100

    done

--- Secure_Shell/04_top_memory_process.sh
+++ Secure_Shell/04_top_memory_process.sh
@@ -4,7 +4,7 @@
 NUM_PROCESSES=5
 
 # Get top memory-consuming processes
-top_processes=$(ps aux --sort -%mem | head -n $((NUM_PROCESSES + 1)) | tail -n $NUM_PROCESSES)
+top_processes=$(ps aux --sort -%mem | head -n $((NUM_PROCESSES + 1)))
 
 # Print top processes
 echo "$top_processes"
