e4e8b69f2a359df864122118270c73ba14a5ff5c
nimaxnimax <avaroti.service@gmail.com>
2024-04-22 18:51:13 +0100

    done

--- Secure_Shell/05_ssh_port_config.sh
+++ Secure_Shell/05_ssh_port_config.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-# Define the new SSH port number
-NEW_PORT=2222
+# Prompt the user to enter the new SSH port number
+read -p "Enter the new SSH port number: " NEW_PORT
 
-# Get the current SSH port number from sshd_config
-CURRENT_PORT=$(grep -Po '^Port\s+\K\d+' /etc/ssh/sshd_config)
+# Check if the entered port number is valid
+if ! [[ "$NEW_PORT" =~ ^[0-9]+$ ]]; then
+    echo "Error: Invalid port number. Please enter a valid port number."
+    exit 1
+fi
 
-# Change the SSH port number in sshd_config
-sudo sed -i "s/^Port .*/Port $NEW_PORT/" /etc/ssh/sshd_config
+# Change SSH port in sshd_config
+sudo sed -i "s/#Port 22/Port $NEW_PORT/" /etc/ssh/sshd_config
 
-# Add the new SSH port to UFW
-sudo ufw allow $NEW_PORT/tcp
-
-# Restart the SSH service
+# Restart SSH service
 sudo systemctl restart ssh
 
-# Print status messages
-echo "SSH port has been changed from $CURRENT_PORT to $NEW_PORT."
-echo "The new port $NEW_PORT has been added to UFW."
+# Print status message
+echo "SSH port has been changed to $NEW_PORT."
 echo "SSH service has been restarted."
 
